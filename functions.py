110dcfe22d7e7a9a1896b548763dee40fde6e47d
Gracormo <146593668+Gracormo@users.noreply.github.com>
2024-12-29 01:23:52 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def desc (df, qname):
+    """
+    This function retrieves a list of questions from a DataFrame based on a specific question name.
+    Parameters:
+        - df: The DataFrame containing the data.
+        - qname: The value in the 'qname' column to filter by.
+    Returns:
+        - A list of questions where the 'qname' column matches the specified qname value.
+    """
+    
+    return list(df.loc[df['qname'] == qname, 'question'])
+
+def count_unique_values(df, col):
+    """
+    Calculate the percentage distribution of unique values in a specified column of a DataFrame.
+
+    This function processes a column of a DataFrame where the values are delimited strings 
+    (e.g., "Python;Java;C++"), splits the strings into individual components, and calculates 
+    the percentage of occurrences of each unique value.
+
+    Parameters:
+        df (pd.DataFrame): The input DataFrame.
+        col (str): The name of the column to analyze.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the unique values and their percentage occurrence.
+    """
+    # Drop rows where the specified column has NaN values
+    df.dropna(subset=[col], inplace=True)
+
+    # Split the delimited strings in the column into lists
+    data_split = df[col].str.split(';')
+
+    # Flatten the lists into a single column
+    data_exploded = data_split.explode()
+
+    # Calculate the percentage occurrences of each unique value
+    data_counts = pd.DataFrame(data_exploded.value_counts() / df.shape[0] *100)
+
+    # Convert the Series to a DataFrame for better presentation
+    result = pd.DataFrame(data_counts)
+
+    return result
+
+
